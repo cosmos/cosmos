9ea7172ee8666166d67f710f94bfdafc7f6ce961
Jae Kwon <jae@tendermint.com>
2016-07-20 11:01:25 -0700

    Add atom_sim

---
+++ atom_sim.py
@@ -0,0 +1,30 @@
+print "Compute atoms for validators and delegators over time"
+
+atomsVal = 0.000    # starting atoms for validator
+atomsDel = 0.010    # starting atoms delegated to validator
+atomsAll = 1.0      #
+inflation = 0.3     # 30% inflation
+exponential = True  # exponential
+commission = 0.15   # 15% commission
+numBlocksPerYear = 1000
+
+for year in range(0,50):
+  rewardsYear = 0.0
+  for i in range(0,numBlocksPerYear):
+    if exponential:
+      blockReward = (atomsAll * inflation) / float(numBlocksPerYear)
+    else:
+      blockReward = inflation / float(numBlocksPerYear)
+
+    atomsAll += blockReward
+    rewardsYear += blockReward
+    rewardVal = blockReward * (atomsVal / atomsAll)
+    rewardDel = blockReward * (atomsDel / atomsAll)
+    rewardVal += rewardDel * commission
+    rewardDel *= (1.0 - commission)
+    atomsVal += rewardVal
+    atomsDel += rewardDel
+    #print atomsVal, atomsDel, (atomsVal / atomsAll)
+  print year, "atomsVal: %0.3f" % (atomsVal,), "atomsDel: %0.3f" % (atomsDel,), \
+  "atomsAll: %0.3f" % (atomsAll,), "atomsVal%%: %0.2f" % ((100 * atomsVal / atomsAll),), \
+  "atomsDel%%: %0.2f" % ((100 * atomsDel / atomsAll),), "rewards: %0.2f"%(rewardsYear,)
